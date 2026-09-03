1e210b9e98f7e07496cc6e8c2fcef6d923605fec
Humphrey Del Castillo <humphreydelcastillo6@gmail.com>
2026-09-03 23:34:06 +0800

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+first_number = int(input("Enter First Number: "))
+second_number = int(input("Enter Second Number: "))
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+print("\nSum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
